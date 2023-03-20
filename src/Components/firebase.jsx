16686265d836f97c53989af3f5a2e0002e96849f
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2023-03-20 10:50:19 -0400

    added env.local file, connected to firebase and stored SDK in firebase.jsx component

---
+++ src/Components/firebase.jsx
@@ -0,0 +1,20 @@
+// Import initializeApp and getDatabase functions
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: "https://jobi-122d7-default-rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const db = getDatabase(firebase);
+
+export { firebase, db };
