8bf1522dc5a7999abff23aae7e37ca7383a8cd63
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2023-04-11 21:05:15 -0400

    mapped through jobPosting locations for filter function and returned them as checkboxes, added styling to open and close location filter with styling

--- src/Components/JobListing/JobSearchForm.jsx
+++ src/Components/JobListing/JobSearchForm.jsx
@@ -1,54 +1,68 @@
 import { useState, useEffect } from "react";
 import JobCard from "./JobCard";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChevronLeft,
+  faFilter,
+  
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+library.add(faChevronLeft, faFilter, faChevronDown);
 
 const JobSearchForm = ({ filteredJobPostings }) => {
-  console.log(filteredJobPostings)
-  const [userInput, setUserInput] = useState('');
-  const [userSelect, setUserSelect] = useState('');
+  console.log(filteredJobPostings, "filtered JOB POSTINGS");
+  const [userInput, setUserInput] = useState("");
+  const [userSelect, setUserSelect] = useState("");
   const [canSubmit, setCanSubmit] = useState(false);
-  
+
+  const [formOpen, setFormOpen] = useState(false);
+
+  const handleFormToggle = () => {
+    setFormOpen(!formOpen);
+  };
 
   const handleChange = (event) => {
-    setUserInput(event.target.value)
-  }
+    setUserInput(event.target.value);
+  };
 
   const handleSelect = (event) => {
     setUserSelect(event.target.value);
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-  }
- 
-  // useEffect for canSubmit status, must be a userInput AND/OR userSelect to perform Search 
-  useEffect(() => {
-    setCanSubmit(userInput && userSelect)
-  }, [userInput, userSelect])
-  
-  
+  };
 
- 
+  // useEffect for canSubmit status, must be a userInput AND userSelect to perform Search
+  useEffect(() => {
+    setCanSubmit(userInput && userSelect);
+  }, [userInput, userSelect]);
+
   return (
     <>
       <div className="content-container">
-        
-      
-
         <h2>Job Listing</h2>
         <h5>We delivered blazing fast & striking work solution</h5>
         <img src="src/assets/box-reg.jpg" alt="" />
         <form action="" className="job-search" onSubmit={handleSubmit}>
           <div className="input-search">
             <label htmlFor="user-search">What are you looking for?</label>
-            <input type="text" id="user-search" placeholder="UI Designer" onChange={handleChange} value={userInput} />
+            <input
+              type="text"
+              id="user-search"
+              placeholder="UI Designer"
+              onChange={handleChange}
+              value={userInput}
+            />
           </div>
           <div className="input-category">
             <label htmlFor="category">Category</label>
             <div className="input-category-flex">
               <select name="category" id="category" onChange={handleSelect}>
-                {console.log(userSelect, 'USER SELECT')}
+                {console.log(userSelect, "USER SELECT")}
                 <option value="Web Design">Web Design</option>
                 {filteredJobPostings &&
                   filteredJobPostings.map((jobPosting, index) => {
@@ -57,24 +71,54 @@ const JobSearchForm = ({ filteredJobPostings }) => {
               </select>
             </div>
           </div>
-          <button disabled={!canSubmit} type="submit" className="job-search-btn">Search</button>
+          <button
+            disabled={!canSubmit}
+            type="submit"
+            className="job-search-btn"
+          >
+            Search
+          </button>
         </form>
       </div>
       <section className="job-search-results">
         <div className="filter-search">
-          <label htmlFor="filter">Filter by</label>
-          <option value=""></option>
-          <select name="filter" id="filter"></select>
+          <div className="filter">
+            <h5>Filter by</h5>
+            <div className="locations">
+              <button onClick={handleFormToggle}>
+                Locations <FontAwesomeIcon icon="fa-solid fa-chevron-down" />
+              </button>
+              <form
+                action=""
+                onSubmit={handleSubmit}
+                className={`form-menu ${formOpen ? "open" : ""}`}
+              >
+                <h5>Locations</h5>
+                {filteredJobPostings.map((job, key) => {
+                  return (
+                    <>
+                      <label key={key}>
+                        <input type="checkbox" /> {job.location}
+                      </label>
+                    </>
+                  );
+                })}
+              </form>
+            </div>
+          </div>
         </div>
         {filteredJobPostings &&
-          filteredJobPostings.filter((item) => {
-            
-            return userInput.toLowerCase() === '' ? item : item.job.toLowerCase().includes(userInput);
-          }).map((jobPosting, index) => {
-            return (
-              <JobCard jobPosting={jobPosting} index={index} key={index} />
-            );
-          })}
+          filteredJobPostings
+            .filter((item) => {
+              return userInput.toLowerCase() === ""
+                ? item
+                : item.job.toLowerCase().includes(userInput);
+            })
+            .map((jobPosting, index) => {
+              return (
+                <JobCard jobPosting={jobPosting} index={index} key={index} />
+              );
+            })}
       </section>
     </>
   );
