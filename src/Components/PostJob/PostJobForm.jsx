75975e09b2a07e0b670aa45d1235a95e0f147121
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2023-04-24 14:26:04 -0400

    imported React Quill, completed HTML structure for page

--- src/Components/PostJob/PostJobForm.jsx
+++ src/Components/PostJob/PostJobForm.jsx
@@ -1,15 +1,148 @@
-// import Navbar from "../Navbar";
-// import Footer from "../Footer"
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
-// const PostJobForm = () => {
-//     return (
-//         <>
-//         <Navbar />
-        
-//         <h1>HEY</h1>
-        
-//         </>
-//     )
-// }
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
 
-// export default PostJobForm;
+import LoginModal from "../LoginModal";
+
+import { ModalProvider } from "../../Contexts/ModalContext";
+
+const PostJobForm = () => {
+  const toolbarOptions = [
+    ["bold", "italic", "underline"],
+    [{ header: [1, 2, false] }],
+    [{ font: [] }],
+    [{ size: ["small", false, "large", "huge"] }],
+    [{ color: [] }, { background: [] }],
+    [{ align: [] }],
+    ["clean"],
+  ];
+
+  const [text, setText] = useState("");
+
+  function handleChange(value) {
+    setText(value);
+  }
+
+  return (
+    <>
+      <ModalProvider>
+        <Navbar />
+        <section className="post-job">
+          <form action="">
+            <h4>Job Details</h4>
+            <div className="job-title">
+              <label>Job Title*</label>
+              <input type="text" placeholder="Ex: Product Designer" required />
+            </div>
+            <div className="job-description">
+              <label htmlFor="">Job Description*</label>
+              <ReactQuill
+                value={text}
+                onChange={handleChange}
+                modules={{ toolbar: toolbarOptions }}
+                placeholder="Write about the job in detail..."
+              />
+            </div>
+            <div className="flex-container">
+                <div className="category">
+                    <label htmlFor="">Job Category</label>
+                    <select name="" id="">
+                        <option value="">Designer</option>
+                    </select>
+                </div>
+                <div className="job-type">
+                    <label htmlFor="">Job Type</label>
+                    <select name="" id="">
+                        <option value="">Full time</option>
+                    </select>
+                </div>
+            </div>
+            <div className="flex-container">
+                <div className="salary">
+                    <label htmlFor="">Salary*</label>
+                    <select name="" id="">
+                        <option value="">Monthly</option>
+                    </select>
+                </div>
+                <div className="min">
+                    <input type="text" placeholder="Min" />
+                    <span>$</span>
+                </div>
+                <div className="max">
+                    <input type="text" placeholder="Max" />
+                    <span>$</span>
+                </div>
+            </div>
+            <h4>Skills & Experience</h4>
+            <div className="skills">
+                <label htmlFor="">Skills*</label>
+                <input type="text" placeholder="Add Skills" />
+            </div>
+            <div className="grid-container">
+                <div className="experience">
+                    <label htmlFor="">Experience*</label>
+                    <select name="" id="">
+                        <option value="">Experience</option>
+                    </select>
+                </div>
+                <div className="qualification">
+                    <label htmlFor="">Qualifications*</label>
+                    <select name="" id="">
+                        <option value="">Qualifications</option>
+                    </select>
+                </div>
+                <div className="industry">
+                    <label htmlFor="">Industry*</label>
+                    <select name="" id="">
+                        <option value="">Select industry</option>
+                    </select>
+                </div>
+                <div className="career-level">
+                    <label htmlFor="">Career Level</label>
+                    <select name="" id="">
+                        <option value="">Select Career</option>
+                    </select>
+                </div>
+            </div>
+            <h4>Address & Location</h4>
+            <div className="address">
+                <label htmlFor="">Address*</label>
+                <input type="text" placeholder="Add Address"/>
+            </div>
+            <div className="grid-container">
+                <div className="country">
+                    <label htmlFor="">Country*</label>
+                    <select name="" id="">
+                        <option value="">Select Country</option>
+                    </select>
+                </div>
+                <div className="city">
+                    <label htmlFor="">City*</label>
+                    <input type="text" placeholder="Enter City" />
+                </div>
+                <div className="state">
+                    <label htmlFor="">State*</label>
+                    <input type="text" placeholder="Enter State" />
+                </div>
+            </div>
+            <div className="map-location">
+                <label htmlFor="">Map Location</label>
+                <input type="text" placeholder="XC23+6XC, Morian,N105" />
+            </div>
+            <div className="button-container">
+                <button>Add</button>
+                <button>Cancel</button>
+            </div>
+          </form>
+        </section>
+
+        <Footer />
+      </ModalProvider>
+    </>
+  );
+};
+
+export default PostJobForm;
